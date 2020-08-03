1208ce9999b5432f657df5826278578e3dae3821
Liem Vo <liemvt@gmail.com>
2020-08-03 15:41:25 +1200

    Use view binding

--- app/src/main/java/com/example/android/hilt/ui/ButtonsFragment.kt
+++ app/src/main/java/com/example/android/hilt/ui/ButtonsFragment.kt
@@ -20,10 +20,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import com.example.android.hilt.R
 import com.example.android.hilt.data.LoggerDataSource
+import com.example.android.hilt.databinding.FragmentButtonsBinding
 import com.example.android.hilt.di.DatabaseLogger
 import com.example.android.hilt.navigator.AppNavigator
 import com.example.android.hilt.navigator.Screens
@@ -40,33 +39,37 @@ class ButtonsFragment : Fragment() {
     @DatabaseLogger
     lateinit var logger: LoggerDataSource
     @Inject lateinit var navigator: AppNavigator
+    
+    private var _binding: FragmentButtonsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_buttons, container, false)
+        _binding = FragmentButtonsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.findViewById<Button>(R.id.button1).setOnClickListener {
+        binding.button1.setOnClickListener {
             logger.addLog("Interaction with 'Button 1'")
         }
-
-        view.findViewById<Button>(R.id.button2).setOnClickListener {
+    
+        binding.button2.setOnClickListener {
             logger.addLog("Interaction with 'Button 2'")
         }
-
-        view.findViewById<Button>(R.id.button3).setOnClickListener {
+    
+        binding.button3.setOnClickListener {
             logger.addLog("Interaction with 'Button 3'")
         }
-
-        view.findViewById<Button>(R.id.all_logs).setOnClickListener {
+    
+        binding.allLogs.setOnClickListener {
             navigator.navigateTo(Screens.LOGS)
         }
-
-        view.findViewById<Button>(R.id.delete_logs).setOnClickListener {
+    
+        binding.deleteLogs.setOnClickListener {
             logger.removeLogs()
         }
     }
--- app/src/main/java/com/example/android/hilt/ui/LogsFragment.kt
+++ app/src/main/java/com/example/android/hilt/ui/LogsFragment.kt
@@ -24,9 +24,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.hilt.R
 import com.example.android.hilt.data.Log
 import com.example.android.hilt.data.LoggerDataSource
+import com.example.android.hilt.databinding.FragmentLogsBinding
+import com.example.android.hilt.databinding.TextRowItemBinding
 import com.example.android.hilt.di.DatabaseLogger
 import com.example.android.hilt.util.DateFormatter
 import dagger.hilt.android.AndroidEntryPoint
@@ -37,38 +38,46 @@ import javax.inject.Inject
  */
 @AndroidEntryPoint
 class LogsFragment : Fragment() {
-
+    
     @Inject
     @DatabaseLogger
     lateinit var logger: LoggerDataSource
-    @Inject lateinit var dateFormatter: DateFormatter
-
+    @Inject
+    lateinit var dateFormatter: DateFormatter
+    
+    private var _binding: FragmentLogsBinding? = null
+    private val binding get() = _binding!!
+    
     private lateinit var recyclerView: RecyclerView
-
+    
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_logs, container, false)
+        _binding = FragmentLogsBinding.inflate(inflater, container, false)
+        return binding.root
     }
-
+    
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view).apply {
-            setHasFixedSize(true)
-        }
+        binding.recyclerView.setHasFixedSize(true)
     }
-
+    
     override fun onResume() {
         super.onResume()
-
+        
         logger.getAllLogs { logs ->
-            recyclerView.adapter =
+            binding.recyclerView.adapter =
                 LogsViewAdapter(
                     logs,
                     dateFormatter
                 )
         }
+    }
+    
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
 
@@ -79,20 +88,19 @@ private class LogsViewAdapter(
     private val logsDataSet: List<Log>,
     private val daterFormatter: DateFormatter
 ) : RecyclerView.Adapter<LogsViewAdapter.LogsViewHolder>() {
-
+    
     class LogsViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
-
+    
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LogsViewHolder {
         return LogsViewHolder(
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.text_row_item, parent, false) as TextView
+            TextRowItemBinding.inflate(LayoutInflater.from(parent.context), parent, false).text
         )
     }
-
+    
     override fun getItemCount(): Int {
         return logsDataSet.size
     }
-
+    
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: LogsViewHolder, position: Int) {
         val log = logsDataSet[position]
